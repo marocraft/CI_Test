b9930e80daa80a71320e551f38969556d64add99
zouhair <jawadi.zouhair.2017@gmail.com>
2018-10-11 17:03:11 +0100

    commit 7

--- build.sh
+++ build.sh
@@ -5,4 +5,15 @@ if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${T
     -Dsonar.projectKey=citestsonar 
     -Dsonar.organization=jzouhair-github 
     -Dsonar.login=${SONAR_LOGIN};
+    elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
+    echo 'Build and analyze pull request'
+    mvn -B clean verify sonar:sonar
+     -Dsonar.host.url=https://sonarcloud.io 
+    -Dsonar.projectKey=citestsonar 
+    -Dsonar.organization=jzouhair-github 
+     -Dsonar.login=${SONAR_LOGIN} 
+     -Dsonar.pullrequest.github.repository=marocraft/CI_Test
+     -Dsonar.pullrequest.provider=GitHub 
+     -Dsonar.pullrequest.branch=${TRAVIS_BRANCH} 
+     -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
 fi
