392292edcd355cc3f6a497b877b9adff3eb28069
zouhair <jawadi.zouhair.2017@gmail.com>
2018-10-11 17:46:06 +0100

    commit 11

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
-if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
+if  [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
     echo 'Checking Quality Gates'
-    mvn -B clean verify sonar:sonar  
-    -Dsonar.host.url=https://sonarcloud.io 
-    -Dsonar.projectKey=citestsonar 
-    -Dsonar.organization=jzouhair-github 
-    -Dsonar.login=${SONAR_LOGIN};
-    elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
-    echo 'Build and analyze pull request'
-    mvn -B clean verify sonar:sonar
-     -Dsonar.host.url=https://sonarcloud.io 
-    -Dsonar.projectKey=citestsonar 
-    -Dsonar.organization=jzouhair-github 
-     -Dsonar.login=${SONAR_LOGIN} 
-     -Dsonar.pullrequest.github.repository=marocraft/CI_Test
-     -Dsonar.pullrequest.provider=GitHub 
-     -Dsonar.pullrequest.branch=${TRAVIS_BRANCH} 
-     -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
+    mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=citestsonar -Dsonar.organization=jzouhair-github -Dsonar.login=${SONAR_LOGIN};
+    
 fi
