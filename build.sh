e996427b57c1fb3de4bc11d1d5551faae67fe9f1
zouhair <jawadi.zouhair.2017@gmail.com>
2018-10-11 17:54:24 +0100

    commit 14

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-
+if  [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
+    echo 'Checking Quality Gates'
     mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=citestsonar -Dsonar.organization=jzouhair-github -Dsonar.login=${SONAR_LOGIN};
-
+    
+fi
